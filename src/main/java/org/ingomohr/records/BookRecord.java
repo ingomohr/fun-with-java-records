54ced5554afe6d9cb07f893d2f88be60c7002ae6
Ingo Mohr <tellastory73@gmail.com>
2024-01-24 20:21:28 +0100

    refactor: add compact constructor to example code

--- src/main/java/org/ingomohr/records/BookRecord.java
+++ src/main/java/org/ingomohr/records/BookRecord.java
@@ -10,8 +10,26 @@ public record BookRecord(String title, int id) {
     // - 2 records are equal if all their components are equal
 
     // What's also possible
+    // - add more constructors
     // - add instance methods
     // - add static methods
     // - add static fields
+
+    // overriding the canonical constructor w/ input validation
+/*     public BookRecord(String title, int id) {
+        if (id < 0) {
+            throw new IllegalArgumentException("Id cannot be negative: " + id);
+        }
+        this.title = title;
+        this.id = id;
+    }
+ */
+    // if we want to modify the canonical constructor, we can use a compact constructor, too
+    public BookRecord {
+        if (id < 0) {
+            throw new IllegalArgumentException("Id cannot be negative: " + id);
+        }
+    }
+
 
 }
